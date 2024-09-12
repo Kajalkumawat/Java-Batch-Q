779665055abbed597c2a6d494f75b6818a8ca033
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-12 13:33:08 +0530

    static local variable in java

---
+++ StaticLocalVariable.java
@@ -0,0 +1,7 @@
+public class StaticLocalVariable {
+    public static void main(String[] args) {
+        // local variable does not static in java
+        static int a = 89;
+        System.out.println(a);
+    }
+}
