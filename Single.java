0abcecd116e5e51f92c5fd56773f8dbc153a2352
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-18 14:39:06 +0530

    single level inheritance in aggregation part of Intherit1

---
+++ Single.java
@@ -0,0 +1,11 @@
+class A {
+    void show() {
+        System.out.println("A class");
+    }
+}
+
+class Single extends A {
+    void show1() {
+        System.out.println("Single class");
+    }
+}
