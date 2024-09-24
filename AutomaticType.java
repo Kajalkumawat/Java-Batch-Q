abd16800945a84f2295272076228b1456287b214
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-24 13:40:50 +0530

    automatic typecasting in java

---
+++ AutomaticType.java
@@ -0,0 +1,14 @@
+public class AutomaticType {
+    public static void main(String[] args) {
+        byte a = 89;
+        int b = a;
+        System.out.println(a + " " + b);
+        long c = 7855;
+        double d = c;
+        System.out.println(c + " " + d);
+
+        char e = 'a';
+        int f = e;
+        System.out.println(e + " " + f);
+    }
+}
