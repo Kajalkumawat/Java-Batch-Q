815b2e758e916efab7e7f48284e3c98192894434
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-27 13:48:29 +0530

    print all the data through loop

---
+++ LoopInArray.java
@@ -0,0 +1,8 @@
+public class LoopInArray {
+    public static void main(String[] args) {
+        int a[] = { 78, 45, 24, 67, 34, 2, 78, 8 };
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
