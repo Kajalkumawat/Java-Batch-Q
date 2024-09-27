55a841cfd12958fb4df0b3f3dd21d55caf306eca
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-27 13:23:22 +0530

    find length of the array through predefine method

---
+++ LengthArray.java
@@ -0,0 +1,7 @@
+public class LengthArray {
+    public static void main(String[] args) {
+        int a[] = { 31, 8, 96, 34, 23, 33, 45, 58 };
+        // find length of the array
+        System.out.println(a.length);
+    }
+}
