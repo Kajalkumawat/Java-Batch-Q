24a9db33fd6f888c3e855c758529c13829245809
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-27 13:34:48 +0530

    dynamic array creation and access the data random

---
+++ Dynmic.java
@@ -0,0 +1,14 @@
+public class Dynmic {
+    public static void main(String[] args) {
+        int a[] = new int[6];// int[size]
+        // data store index 0,1,2,3,4,5
+        // length :6
+        a[0] = 34;
+        a[1] = 89;
+        a[2] = 44;
+        a[3] = 78;
+        a[4] = 1;
+        a[5] = 89;
+        System.out.println(a[4]+" "+a[1]);
+    }
+}
