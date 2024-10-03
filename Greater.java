86e7242d8a1f042d8f3378047f5980df2830e079
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-03 13:43:07 +0530

    INPUT ARRAY AND CHECK STRICTLY GREATER THAN TARGET

---
+++ Greater.java
@@ -0,0 +1,11 @@
+public class Greater {
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int target = 5;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > target) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
