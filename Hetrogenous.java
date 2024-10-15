fe95cdf71bcb5eddf54413b92aaf13e2bdad7213
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-15 12:51:01 +0530

    hetrogenous array

---
+++ Hetrogenous.java
@@ -0,0 +1,9 @@
+public class Hetrogenous {
+
+    public static void main(String[] args) {
+        Object a[] = { "hello", 76, 'j' };
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
