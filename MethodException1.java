eaaa70c617bf75eb05eb6bbb6aa8e39100c2b876
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-23 12:38:20 +0530

    PRINT REFRENCE VARIABLE IN JAVA

---
+++ MethodException1.java
@@ -0,0 +1,11 @@
+public class MethodException1 {
+    public static void main(String[] args) {
+        try{
+            int a=9/0;
+            System.out.println(a);
+        }
+        catch(ArithmeticException ex){
+           System.out.println(ex);
+        }
+    }
+}
