65ac39d969aa42cf8ef2d239eb541df6c3cd4953
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 12:35:38 +0530

    THROW CASE 4

---
+++ ThrowCase4.java
@@ -0,0 +1,8 @@
+public class ThrowCase4 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        throw new ArithmeticException("kajal kumawat");
+        System.out.println("world");
+        // error :unreachable statement error
+    }
+}
