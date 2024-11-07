4197d0e4b72ee7609ad9437281d779fcce66cb0f
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-07 12:39:01 +0530

    THROW CASE THREE IN JAVA

---
+++ ThrowsCase3.java
@@ -0,0 +1,10 @@
+public class ThrowsCase3 {
+    // throw with uncheck exception
+    static void show() throws ArithmeticException {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        show();
+    }
+}
