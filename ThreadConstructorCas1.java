4842e66d3381a89858ea59e3773a2b4efe19fe6c
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-19 09:28:06 +0530

    DEFAULT CONSTRUCTOR OF THREAD CLASS

---
+++ ThreadConstructorCas1.java
@@ -0,0 +1,8 @@
+public class ThreadConstructorCas1 {
+    public static void main(String[] args) {
+        System.out.println("default thread constructor");
+        // 1)Thread t=new Thread();
+        Thread t = new Thread();
+
+    }
+}
