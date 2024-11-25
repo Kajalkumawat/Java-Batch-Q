5c62e824489221edf04020edd1da1c19a5f9cffa
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-25 16:45:24 +0530

    creation of string in java

---
+++ StringConstant.java
@@ -0,0 +1,11 @@
+public class StringConstant {
+    public static void main(String[] args) {
+        // string constant
+        String s = "hello world";
+        System.out.println(s);
+
+        // string dynamic :
+        String s1 = new String("regex is learning ");
+        System.out.println(s1);
+    }
+}
