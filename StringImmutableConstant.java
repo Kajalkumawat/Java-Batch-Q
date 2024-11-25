d6e3e0b704817918b819c632e622ec597ebcff42
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-25 17:00:40 +0530

    StringImmutableConstant

---
+++ StringImmutableConstant.java
@@ -0,0 +1,7 @@
+public class StringImmutableConstant {
+    public static void main(String[] args) {
+        String s = "hello";
+        s.concat("world");
+        System.out.println(s);
+    }
+}
