ded3b896bcfb828cc488c8930e199ebb5f5ef67d
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-25 17:22:26 +0530

    COUNT STRING CONSTANT POOL AND HEAP MEMORY

---
+++ StringSCP.java
@@ -0,0 +1,12 @@
+public class StringSCP {
+    public static void main(String[] args) {
+        String s = "hello";
+        String s1 = new String("hii");
+        String s2 = new String("world");
+        String s3 = new String("hi");
+        String s4 = "bye";
+        // total number of SCP and Heap 
+        //SCP :5 
+        //heap :3
+    }
+}
