3f09ce7636e7ca295d50e404e0307960a7bb2a59
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-25 17:36:49 +0530

    StringBuilderBasic

---
+++ StringBuilderBasic.java
@@ -0,0 +1,6 @@
+public class StringBuilderBasic {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello world");
+        System.out.println(sb);
+    }
+}
