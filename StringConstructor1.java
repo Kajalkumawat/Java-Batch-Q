259bdccd0c464ced366c4a31395aa14920ae631f
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-27 16:55:12 +0530

    STRING DEFAULT CONSTRUCTOR IN JAVA

---
+++ StringConstructor1.java
@@ -0,0 +1,7 @@
+public class StringConstructor1 {
+    public static void main(String[] args) {
+        // a)String s=new String();
+        String s = new String();
+        System.out.println(s);
+    }
+}
