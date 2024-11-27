2ed3a0306161bf07e7ce9b334fccd0b893f95a8f
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-27 17:16:23 +0530

    BYTE ARRAY CONSTRUCTOR IN JAVA

---
+++ StringConstructor5.java
@@ -0,0 +1,8 @@
+public class StringConstructor5 {
+    public static void main(String[] args) {
+        // String s=new String(byte a);
+        byte a[] = { 4, 6 };
+        String s = new String(a);
+        System.out.println(s);
+    }
+}
