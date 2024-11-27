5f69523c886ee5bcd3b75bc91018a38694e493c1
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-27 16:58:04 +0530

    STRING CONSTRUCTOR IN JAVA

---
+++ StringConstructor2.java
@@ -0,0 +1,8 @@
+public class StringConstructor2 {
+    public static void main(String[] args) {
+        // String s=new String(String s1);
+        String s1 = new String("hello world");
+        String s2 = new String(s1);
+        System.out.println(s2);
+    }              
+}
