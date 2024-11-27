3ab9816cf3f1a8cb792b36f8b6e3240f464bdc5b
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-27 17:20:03 +0530

    CHARACTER TYPE STRING CONSTRUCTOR IN JAVA

---
+++ StringConstructor6.java
@@ -0,0 +1,8 @@
+public class StringConstructor6 {
+    public static void main(String[] args) {
+        // String s=new String (char a[]);
+        char a[] = { 'a', 'p', 'p', 'l', 'e' };
+        String s = new String(a);
+        System.out.println(s);
+    }
+}
