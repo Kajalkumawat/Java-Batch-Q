19183a2be31d1b8bf7eb535bb86eb590bbbce52c
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-28 17:35:04 +0530

    StringBufferConstructor2

---
+++ StringBufferConstructor2.java
@@ -0,0 +1,8 @@
+public class StringBufferConstructor2 {
+    public static void main(String[] args) {
+        // b)StringBuffer sb=new StringBuffer(String s);
+        String s = "hello";
+        StringBuffer sb = new StringBuffer(s);
+        System.out.println(sb);
+    }
+}
