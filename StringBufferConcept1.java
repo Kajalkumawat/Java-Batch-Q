731bd174e5a4a574517e2ec7890f24c5a99c1131
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-28 17:14:40 +0530

    StringBufferConcept1

---
+++ StringBufferConcept1.java
@@ -0,0 +1,8 @@
+public class StringBufferConcept1 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello");
+        // append method :is string buffer method
+        sb.append("world");
+        System.out.println(sb);
+    }
+}
