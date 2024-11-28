ccfd9b2c27ee8eede7a78c3055647f32d458e93d
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-28 17:36:37 +0530

    StringBufferConstructor3

---
+++ StringBufferConstructor3.java
@@ -0,0 +1,7 @@
+public class StringBufferConstructor3 {
+    public static void main(String[] args) {
+        // c)StringBuffer sb=new StringBUffer(int capacity);
+        StringBuffer sb = new StringBuffer(89);
+        System.out.println(sb);
+    }
+}
