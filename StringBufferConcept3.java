a5ec2aa5d31f4f0fd30dfe4c463210d60263b5c2
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-28 17:24:51 +0530

    StringBufferConcept3

---
+++ StringBufferConcept3.java
@@ -0,0 +1,11 @@
+public class StringBufferConcept3 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello");
+        sb.append("world");
+        System.out.println(sb);
+        sb = new StringBuffer("duniya");
+
+        System.out.println(sb);
+
+    }
+}
