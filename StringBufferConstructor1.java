8926c4b165890c14c9e33610098deb147393146f
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-28 17:33:45 +0530

    StringBufferConstructor1

---
+++ StringBufferConstructor1.java
@@ -0,0 +1,7 @@
+public class StringBufferConstructor1 {
+    public static void main(String[] args) {
+        // a)StringBuffer sb=new StringBuffer();
+        StringBuffer sb = new StringBuffer();
+        System.out.println(sb);
+    }
+}
