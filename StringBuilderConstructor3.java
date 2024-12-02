81e12ea0944c9714875997a7d11af6907c7186c3
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-02 16:57:39 +0530

    STRING CONSTRUCTOR IN STRING BUILDER

---
+++ StringBuilderConstructor3.java
@@ -0,0 +1,8 @@
+public class StringBuilderConstructor3 {
+    public static void main(String[] args) {
+        // StringBuilder sb=new StringBuilder(String s);
+        String s = "hello world";
+        StringBuilder sb = new StringBuilder(s);
+        System.out.println(sb);
+    }
+}
