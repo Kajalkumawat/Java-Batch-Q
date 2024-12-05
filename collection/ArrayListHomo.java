d46e58274243bd591a2350c829bd7969c624a3d9
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-05 17:42:40 +0530

    arraylist homogenous in java

---
+++ collection/ArrayListHomo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ArrayListHomo {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(45);
+        al.add(12);
+        al.add(98);
+        System.out.println(al);
+    }
+}
