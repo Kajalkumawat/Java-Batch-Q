551844967276e246c02bea4087120db4e12faf48
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-17 16:47:25 +0530

    generic linkedlist

---
+++ collection/linkedlist/GenericList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class GenericList {
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("hello");
+        ll.add("hii");
+        ll.add("bye");
+        System.out.println(ll);
+    }
+}
