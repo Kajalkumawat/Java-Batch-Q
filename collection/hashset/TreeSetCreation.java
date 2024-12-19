fb0760679635f36b0694fbc479bc3c9b70f6c8bf
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-19 17:22:43 +0530

    class cast exception in java

---
+++ collection/hashset/TreeSetCreation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class TreeSetCreation {
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet<>();
+        ts.add(45);
+        ts.add("hello");
+        ts.add(78.6);
+        System.out.println(ts);
+    }
+}
