d7e74260e3ddd890c7f702fab123211e95aa80e0
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-23 16:59:08 +0530

    hash map basic in java

---
+++ collection/hashmap/HashMapBasic1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HashMapBasic1{
+    public static void main(String[] args){
+        //hashmap is work on key and value 
+        HashMap<Integer,String> hs=new HashMap<>();
+        hs.put(1, "hello");
+        hs.put(2,"bye");
+        System.out.println(hs);
+    }
+}
