2f5e2e4a5b0a23c5644b5372a4ae930f49c24d71
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-23 17:07:38 +0530

    implementation through interface in java

---
+++ collection/hashmap/HashMapWays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HashMapWays {
+    public static void main(String[] args) {
+        HashMap<Integer,String> hs1=new HashMap<>();
+        hs1.put(1,"one");
+        hs1.put(2,"bye");
+        //map through implement hashmap
+        Map<String,Integer> hs2=new HashMap<>();
+        hs2.put("hello", 56);
+        hs2.put("world", 78);
+        System.out.println(hs2);
+    }
+}
