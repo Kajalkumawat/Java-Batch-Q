025bcba6a3737aad88c39092744a6a7186fb8ade
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-23 17:23:14 +0530

    hash map method in java

---
+++ collection/hashmap/HashMapMethod.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HashMapMethod {
+    public static void main(String[] args) {
+        HashMap<Integer,String> hs1=new HashMap<>();
+        hs1.put(1, "hello");
+        hs1.put(2, "byee");
+        hs1.put(3, "tata");
+        System.out.println(hs1.keySet());
+    }
+}
