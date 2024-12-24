7ce70a53757e3b9027157ee3652f7d175fe200fd
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-24 16:52:38 +0530

    linked hash map in java

---
+++ collection/hashmap/LinkedHashMapBasic1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class LinkedHashMapBasic1 {
+    public static void main(String[] args) {
+       LinkedHashMap lhs=new LinkedHashMap<>();
+       lhs.put(5,"hello") ;
+       lhs.put(4.5,'j');
+       System.out.println(lhs);
+    }
+}
