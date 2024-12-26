1e7638d318d3305a9881006caf970836f1bd4393
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-26 17:20:02 +0530

    priority queue implementation in java

---
+++ collection/hashmap/PriorityQueueBasic1.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+import java.util.*;
+public class PriorityQueueBasic1 {
+    public static void main(String[] args) {
+        //implementation : 
+        Iterable<Integer> itr=new PriorityQueue<>();
+
+        Collection<Integer> c=new PriorityQueue<>();
+        c.add(34);
+
+          Queue<Integer> q=new PriorityQueue<>();
+          q.add(45);
+
+    }
+}
+
