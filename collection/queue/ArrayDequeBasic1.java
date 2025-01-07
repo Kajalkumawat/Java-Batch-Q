00e8d108d5669205a56e8385d6091b7e27e8f2e8
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-01-07 09:31:30 +0530

    queue implementation through array dequeue in java

---
+++ collection/queue/ArrayDequeBasic1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ArrayDequeBasic1 {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        ad.add(12);
+        ad.add(56);
+        System.out.println(ad);
+    }
+}
