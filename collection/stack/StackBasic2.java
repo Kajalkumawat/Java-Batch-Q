9bfefa43d2b0e4e5de37b28d2fe3858337c7251a
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-01-09 14:32:26 +0530

    stack peek method in java

---
+++ collection/stack/StackBasic2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StackBasic2 {
+    public static void main(String[] args) {
+       Stack<Integer>st =new Stack<>();
+       st.push(1);
+       st.push(2);
+       st.push(3);
+       System.out.println(st.peek());
+    }
+}
