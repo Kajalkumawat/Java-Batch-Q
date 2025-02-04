c98414732be666e2438712841b16d9b7f487b218
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-04 12:20:46 +0530

    return statement in java

---
+++ DSA/recurssion/Return1.java
@@ -0,0 +1,10 @@
+public class Return1 {
+    static void returnstatement(){
+        System.out.println("upper side");
+        return;
+        //System.out.println("lower side");
+    }
+    public static void main(String[] args){
+        Return1.returnstatement();
+    }
+}
