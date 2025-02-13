05e29a56f643d8260e3edc461d0841008c0084dc
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-13 12:07:44 +0530

    print value of the node

---
+++ DSA/linked list/CreateNode1.java
@@ -0,0 +1,16 @@
+public class CreateNode1 {
+    static class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value=value;
+        }
+    }
+    public static void main(String[] args) {
+        //3 node create 
+        Node a=new Node(12);
+        Node b=new Node(23);
+        Node c=new Node(78);
+        System.out.println(a.value+" "+b.value+" "+c.value);
+    }
+}
